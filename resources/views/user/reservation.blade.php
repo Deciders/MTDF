24b6b9f89bf0faaf000c1ce2803057f8663559cb
Udeshika Madushani <udeshi.um92@gmail.com>
2018-02-07 13:53:02 +0530

    User DashBoard 1 s commite
    user views deifes
    
    Co-Authored-By: Udeshika Madushani <udeshi92@users.noreply.github.com>

---
+++ resources/views/user/reservation.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>Reservation</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+		<div class="col-xs-12">
+		  <h3>Nav Tab left Side </h3>
+			<!-- tabs -->
+			<div class="tabbable tabs-left">
+				<ul class="nav nav-tabs">
+					<li class="active"><a href="#home" data-toggle="tab">Home</a></li>
+					<li><a href="#home" data-toggle="tab">Samsung</a></li>
+					<li><a href="#about" data-toggle="tab">Huawei</a></li>
+					<li><a href="#contact" data-toggle="tab">HTC</a></li>
+				</ul>
+				<div class="tab-content">
+					<div class="tab-pane active" id="home">                
+						<div class="">
+							<h1>Samsung</h1>
+							<p>These lists are meant to identify articles which deserve editor attention because they are the most important for an encyclopedia to have, as determined by the community of participating editors. They may also be of interest to readers as an alternative to lists of overview articles.</p>                 
+						</div>
+					</div> 
+					<div class="tab-pane" id="about"> 
+						<div class="">
+							<h1>Huawei</h1>
+							<p>because they are the most important for an encyclopedia to have, as determined by the community of participating editors. They may also be of interest to readers as an alternative to lists of overview articles.</p>                 
+						</div>
+					</div>
+				
+					<div class="tab-pane" id="services"> 
+						<div class="">
+							<h1>HTC</h1>
+							<p>meant to identify articles which deserve editor attention because they are the most important for an encyclopedia to have, as determined by the community of participating editors. They may also be of interest to readers as an alternative to lists of overview articles.</p>                 
+						</div>
+					</div>
+				
+					<div class="tab-pane" id="contact"> 
+						 <div class="">
+							<h1>Contact Tab</h1>
+							<p>deserve editor attention because they are the most important for an encyclopedia to have, as determined by the community of participating editors. They may also be of interest to readers as an alternative to lists of overview articles.</p>                 
+						 </div>
+					</div>
+				</div>
+			</div>
+			<!-- /tabs -->
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
